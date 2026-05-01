09d6ab89dec8629f0499b062643d16e6eb7c22b8
Jefferson Wu <jeffersonw888@gmail.com>
2026-05-01 15:10:51 -0500

    addTroops_negativeTroops_fail passes

--- src/main/java/domain/Territory.java
+++ src/main/java/domain/Territory.java
@@ -1,4 +1,18 @@
 package domain;
 
 public class Territory {
+    private Player owner;
+    private int troopCount;
+
+    Territory(Player owner, int troopCount) {
+        this.owner = owner;
+        this.troopCount = troopCount;
+    }
+
+    public void addTroops(int amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Input must be non-negative integer");
+        }
+    }
+
 }
--- src/test/java/domain/TerritoryTests.java
+++ src/test/java/domain/TerritoryTests.java
@@ -1,4 +1,26 @@
 package domain;
 
+import org.easymock.EasyMock;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class TerritoryTests {
+
+    @Test
+    public void addTroops_negativeTroops_fail(){
+        Player p1  = EasyMock.createMock(Player.class);
+        Territory t1 = new Territory(p1, 5);
+
+        int input = -1;
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            t1.addTroops(input);
+        });
+
+        String expectedMessage = "Input must be non-negative integer";
+        assertEquals(expectedMessage, exception.getMessage());
+    }
+
+
 }

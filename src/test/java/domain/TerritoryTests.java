9148243c26745f04fa212513c0a28ba69d3faaa0
brockbrown-nu <brockbrown2027@u.northwestern.edu>
2026-05-17 18:18:55 -0500

    fix linter errors by making indentation 2 spaces

--- src/test/java/domain/TerritoryTests.java
+++ src/test/java/domain/TerritoryTests.java
@@ -8,159 +8,159 @@ import org.junit.jupiter.api.Test;
 
 public class TerritoryTests {
 
-    @Test
-    public void constructor_nullName_throwsIllegalArgumentException() {
-        Player p1 = EasyMock.createMock(Player.class);
-        assertThrows(IllegalArgumentException.class, () -> new Territory(null, p1, 5));
-    }
+  @Test
+  public void constructor_nullName_throwsIllegalArgumentException() {
+    Player p1 = EasyMock.createMock(Player.class);
+    assertThrows(IllegalArgumentException.class, () -> new Territory(null, p1, 5));
+  }
 
-    @Test
-    public void constructor_emptyName_throwsIllegalArgumentException() {
-        Player p1 = EasyMock.createMock(Player.class);
-        assertThrows(IllegalArgumentException.class, () -> new Territory("", p1, 5));
-    }
+  @Test
+  public void constructor_emptyName_throwsIllegalArgumentException() {
+    Player p1 = EasyMock.createMock(Player.class);
+    assertThrows(IllegalArgumentException.class, () -> new Territory("", p1, 5));
+  }
 
-    @Test
-    public void constructor_validName_successfulNameAssignment() {
-        Player p1 = EasyMock.createMock(Player.class);
-        Territory t1 = new Territory("TestTerritory", p1, 5);
+  @Test
+  public void constructor_validName_successfulNameAssignment() {
+    Player p1 = EasyMock.createMock(Player.class);
+    Territory t1 = new Territory("TestTerritory", p1, 5);
 
-        String expected = "TestTerritory";
-        assertEquals(expected, t1.getName());
-    }
+    String expected = "TestTerritory";
+    assertEquals(expected, t1.getName());
+  }
 
-    @Test
-    public void constructor_nullPlayer_throwsIllegalArgumentException() {
-        assertThrows(IllegalArgumentException.class, () ->
-                new Territory("TestTerritory", null, 5));
-    }
+  @Test
+  public void constructor_nullPlayer_throwsIllegalArgumentException() {
+    assertThrows(IllegalArgumentException.class, () ->
+        new Territory("TestTerritory", null, 5));
+  }
 
-    @Test
-    public void constructor_validPlayer_successfulPlayerAssignment() {
-        Player p1 = EasyMock.createMock(Player.class);
-        Territory t1 = new Territory("TestTerritory", p1, 5);
+  @Test
+  public void constructor_validPlayer_successfulPlayerAssignment() {
+    Player p1 = EasyMock.createMock(Player.class);
+    Territory t1 = new Territory("TestTerritory", p1, 5);
 
-        assertEquals(t1.getOwner(), p1);
-    }
+    assertEquals(t1.getOwner(), p1);
+  }
 
-    @Test
-    public void addTroops_negativeTroops_fail(){
-        Player p1 = EasyMock.createMock(Player.class);
-        Territory t1 = new Territory("TestTerritory", p1, 5);
-    
-        int input = -1;
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            t1.addTroops(input);
-        });
-    
-        String expectedMessage = "Input must be non-negative integer";
-        assertEquals(expectedMessage, exception.getMessage());
-    }
-    
-    @Test
-    public void addTroops_zeroTroops_fail() {
-        Player p1 = EasyMock.createMock(Player.class);
-        Territory t1 = new Territory("TestTerritory", p1, 0);
-    
-        int input = 0;
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            t1.addTroops(input);
-        });
-    
-        String expectedMessage = "Input must be non-negative integer";
-        assertEquals(expectedMessage, exception.getMessage());
-    }
+  @Test
+  public void addTroops_negativeTroops_fail() {
+    Player p1 = EasyMock.createMock(Player.class);
+    Territory t1 = new Territory("TestTerritory", p1, 5);
 
-    @Test
-    public void addTroops_oneTroop_success() {
-        Player p1 = EasyMock.createMock(Player.class);
-        Territory t1 = new Territory("TestTerritory", p1, 1);
+    int input = -1;
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+      t1.addTroops(input);
+    });
 
-        int input = 1;
-        int expected = 2;
+    String expectedMessage = "Input must be non-negative integer";
+    assertEquals(expectedMessage, exception.getMessage());
+  }
 
-        t1.addTroops(input);
+  @Test
+  public void addTroops_zeroTroops_fail() {
+    Player p1 = EasyMock.createMock(Player.class);
+    Territory t1 = new Territory("TestTerritory", p1, 0);
 
-        assertEquals(expected, t1.getTroopCount());
-    }
+    int input = 0;
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+      t1.addTroops(input);
+    });
 
-    @Test
-    public void removeTroops_oneTroop_removeOneTroop_fail() {
-        Player p1 = EasyMock.createMock(Player.class);
-        Territory t1 = new Territory("TestTerritory", p1, 1);
+    String expectedMessage = "Input must be non-negative integer";
+    assertEquals(expectedMessage, exception.getMessage());
+  }
 
-        int input = 1;
+  @Test
+  public void addTroops_oneTroop_success() {
+    Player p1 = EasyMock.createMock(Player.class);
+    Territory t1 = new Territory("TestTerritory", p1, 1);
 
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            t1.removeTroops(input);
-        });
+    int input = 1;
+    int expected = 2;
 
-        String expectedMessage = "Territories must have at least 1 troop";
-        assertEquals(expectedMessage, exception.getMessage());
-    }
+    t1.addTroops(input);
 
-    @Test
-    public void removeTroops_oneTroop_removeTwoTroop_fail() {
-        Player p1 = EasyMock.createMock(Player.class);
-        Territory t1 = new Territory("TestTerritory", p1, 1);
+    assertEquals(expected, t1.getTroopCount());
+  }
 
-        int input = 2;
+  @Test
+  public void removeTroops_oneTroop_removeOneTroop_fail() {
+    Player p1 = EasyMock.createMock(Player.class);
+    Territory t1 = new Territory("TestTerritory", p1, 1);
 
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            t1.removeTroops(input);
-        });
+    int input = 1;
 
-        String expectedMessage = "Territories must have at least 1 troop";
-        assertEquals(expectedMessage, exception.getMessage());
-    }
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+      t1.removeTroops(input);
+    });
 
-    @Test
-    public void removeTroops_FiveTroops_removeFourTroops_success() {
-        Player p1 = EasyMock.createMock(Player.class);
-        Territory t1 = new Territory("TestTerritory", p1, 5);
+    String expectedMessage = "Territories must have at least 1 troop";
+    assertEquals(expectedMessage, exception.getMessage());
+  }
 
-        int input = 4;
-        t1.removeTroops(input);
+  @Test
+  public void removeTroops_oneTroop_removeTwoTroop_fail() {
+    Player p1 = EasyMock.createMock(Player.class);
+    Territory t1 = new Territory("TestTerritory", p1, 1);
 
-        assertEquals(1, t1.getTroopCount());
-    }
+    int input = 2;
 
-    @Test
-    public void setOwner_P2SetsOwnerOnP1Territory_ownerIsP2() {
-        Player p1 = EasyMock.createMock(Player.class);
-        Player p2 = EasyMock.createMock(Player.class);
-        Territory t1 = new Territory("TestTerritory", p1, 5);
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+      t1.removeTroops(input);
+    });
 
-        t1.setOwner(p2);
+    String expectedMessage = "Territories must have at least 1 troop";
+    assertEquals(expectedMessage, exception.getMessage());
+  }
 
-        assertEquals(p2, t1.getOwner());
-    }
+  @Test
+  public void removeTroops_FiveTroops_removeFourTroops_success() {
+    Player p1 = EasyMock.createMock(Player.class);
+    Territory t1 = new Territory("TestTerritory", p1, 5);
 
-    @Test
-    public void conquer_P2ConquersP1_movesZeroTroops_fail() {
-        Player p1 = EasyMock.createMock(Player.class);
-        Player p2 = EasyMock.createMock(Player.class);
-        Territory t1 = new Territory("TestTerritory", p1, 5);
+    int input = 4;
+    t1.removeTroops(input);
 
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            t1.conquer(p2, 0);
-        });
+    assertEquals(1, t1.getTroopCount());
+  }
 
-        String expectedMessage = "Conquered Territories must have at least 1 troop";
-        assertEquals(expectedMessage, exception.getMessage());
-    }
+  @Test
+  public void setOwner_P2SetsOwnerOnP1Territory_ownerIsP2() {
+    Player p1 = EasyMock.createMock(Player.class);
+    Player p2 = EasyMock.createMock(Player.class);
+    Territory t1 = new Territory("TestTerritory", p1, 5);
 
-    @Test
-    public void conquer_P2ConquersP1_movesOneTroops_success() {
-        Player p1 = EasyMock.createMock(Player.class);
-        Player p2 = EasyMock.createMock(Player.class);
-        Territory t1 = new Territory("TestTerritory", p1, 5);
+    t1.setOwner(p2);
 
-        t1.conquer(p2, 1);
+    assertEquals(p2, t1.getOwner());
+  }
 
-        int expectedTroops = 1;
-        assertEquals(p2, t1.getOwner());
-        assertEquals(expectedTroops, t1.getTroopCount());
-    }
+  @Test
+  public void conquer_P2ConquersP1_movesZeroTroops_fail() {
+    Player p1 = EasyMock.createMock(Player.class);
+    Player p2 = EasyMock.createMock(Player.class);
+    Territory t1 = new Territory("TestTerritory", p1, 5);
+
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+      t1.conquer(p2, 0);
+    });
+
+    String expectedMessage = "Conquered Territories must have at least 1 troop";
+    assertEquals(expectedMessage, exception.getMessage());
+  }
+
+  @Test
+  public void conquer_P2ConquersP1_movesOneTroops_success() {
+    Player p1 = EasyMock.createMock(Player.class);
+    Player p2 = EasyMock.createMock(Player.class);
+    Territory t1 = new Territory("TestTerritory", p1, 5);
+
+    t1.conquer(p2, 1);
+
+    int expectedTroops = 1;
+    assertEquals(p2, t1.getOwner());
+    assertEquals(expectedTroops, t1.getTroopCount());
+  }
 
 }

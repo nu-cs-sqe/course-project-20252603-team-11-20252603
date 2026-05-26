13006a75f2d99c98ca36a7217ce2b3e8ec3e3728
Kris Yun <ksyun360@gmail.com>
2026-05-25 19:32:37 -0500

    feat(ui): convert Main stub into JavaFX Application

--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -1,5 +1,20 @@
 package ui;
 
-public class Main {
-  public static void main(String[] args) {}
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+  @Override
+  public void start(Stage primaryStage) {
+    primaryStage.setTitle("ATLA Risk");
+    primaryStage.setScene(new Scene(new StackPane(), 800, 600));
+    primaryStage.show();
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
 }

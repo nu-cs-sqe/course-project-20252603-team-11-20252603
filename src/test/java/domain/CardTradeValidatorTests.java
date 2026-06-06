ed6cc33da92df98d54d262951f98c49861b9ed51
NanDhanesh <93307274+NanDhanesh@users.noreply.github.com>
2026-06-06 12:58:25 -0700

    isValidSet_threeCavalryCards_returnsTrue passes

--- src/test/java/domain/CardTradeValidatorTests.java
+++ src/test/java/domain/CardTradeValidatorTests.java
@@ -53,4 +53,20 @@ public class CardTradeValidatorTests {
 
     EasyMock.verify(card1, card2, card3);
   }
+
+  @Test
+  public void isValidSet_threeCavalryCards_returnsTrue() {
+    RiskCard card1 = EasyMock.createMock(RiskCard.class);
+    RiskCard card2 = EasyMock.createMock(RiskCard.class);
+    RiskCard card3 = EasyMock.createMock(RiskCard.class);
+    EasyMock.expect(card1.getType()).andReturn(RiskCardType.CAVALRY).anyTimes();
+    EasyMock.expect(card2.getType()).andReturn(RiskCardType.CAVALRY).anyTimes();
+    EasyMock.expect(card3.getType()).andReturn(RiskCardType.CAVALRY).anyTimes();
+    EasyMock.replay(card1, card2, card3);
+
+    CardTradeValidator validator = new CardTradeValidator();
+    assertTrue(validator.isValidSet(List.of(card1, card2, card3)));
+
+    EasyMock.verify(card1, card2, card3);
+  }
 }
